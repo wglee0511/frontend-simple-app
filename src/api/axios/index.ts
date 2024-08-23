4b062f027fcd92dceb9af7d0366eec5c12496cca
wglee0511 <ranazang@naver.com>
2024-08-23 19:00:39 +0900

    feature: main lib

---
+++ src/api/axios/index.ts
@@ -0,0 +1,5 @@
+import coinInfoAxios from './coinInfoAxios';
+import coinMeerkatAxios from './coinMeerkatAxios';
+import traderKatAxios from './traderKatAxios'
+
+export const axiosList = [coinMeerkatAxios, coinInfoAxios, traderKatAxios];
---
+++ src/lib/constant.ts
@@ -0,0 +1,4 @@
+const { REACT_APP_API_KEY, REACT_APP_MAIN_URL } = process.env;
+
+export const IMAGE_API_KEY = REACT_APP_API_KEY;
+export const MAIN_URL = REACT_APP_MAIN_URL;
